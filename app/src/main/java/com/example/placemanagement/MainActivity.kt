38e4c2075ecaa1c22a445c402b8483fb32c1c874
IT21227486 <it21227486@my.sliit.lk>
2023-05-02 13:14:30 +0530

    modify main activity Commit

--- app/src/main/java/com/example/placemanagement/MainActivity.kt
+++ app/src/main/java/com/example/placemanagement/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.placemanagement
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val sigiriyaBtn = findViewById<Button>(R.id.sigiriya)
+        sigiriyaBtn.setOnClickListener {
+            val Intent = Intent (this, sigiriya::class.java)
+            startActivity(Intent)
+        }
+
     }
 }
